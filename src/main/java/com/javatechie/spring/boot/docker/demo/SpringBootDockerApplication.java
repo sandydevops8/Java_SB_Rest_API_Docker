725b3b26f83629ae350c920b7ef370d904ef6853
sandeshlamsal <qalamsal8@gmail.com>
2023-07-19 16:13:24 -0500

    initial commit

---
+++ src/main/java/com/javatechie/spring/boot/docker/demo/SpringBootDockerApplication.java
@@ -0,0 +1,21 @@
+package com.javatechie.spring.boot.docker.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootDockerApplication {
+
+	@GetMapping("/check")
+	public String getMessage() {
+		return "Welcome to Sandy SpringBoot !!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerApplication.class, args);
+	}
+
+}
